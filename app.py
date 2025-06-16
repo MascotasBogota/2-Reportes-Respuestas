297b7f30d8ccbd748410456668c94e79a0759578
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2025-06-16 10:43:31 -0500

    Ultimos cambios

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from config import DevelopmentConfig
 from src.extensions import api, init_db
 from src.routes.report_routes import ns as reports_ns
@@ -7,6 +8,7 @@ from src.routes.response_routes import ns as responses_ns  # lo habilitarás má
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app)  # Habilita CORS para la aplicación
 
     # Inicializa base de datos y API Swagger
     init_db(app)
@@ -18,4 +20,4 @@ def create_app(config_class=DevelopmentConfig):
     return app
 
 if __name__ == "__main__":
-    create_app().run(host="0.0.0.0", port=5000)
+    create_app().run(host="0.0.0.0", port=5050)
